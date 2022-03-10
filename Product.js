386cd526e5d9036f7d10da24811f8327117633f1
Afanasiev <afanasiev.lucia@gmail.com>
2022-03-10 10:45:47 +0200

    separate the code that test the objects in a -app.js- file

--- Product.js
+++ Product.js
@@ -15,11 +15,3 @@ class Product {
     price - ${this.price}`);
     }
 }
-
-const apple = new Product('001', 'A', 'Apple-Golden', 'url(image.jpeg)', '10lei/kg');
-const orange = new Product('002', 'A', "Orange", 'url(orange.jpg)', '25lei/kg');
-const avocado = new Product('003', "A", 'Avocado', 'url(avocado.img)', '75lei/kg');
-
-apple.log();
-orange.log();
-avocado.log()
---
+++ app.js
@@ -0,0 +1,7 @@
+const apple = new Product('001', 'A', 'Apple-Golden', 'url(image.jpeg)', '10lei/kg');
+const orange = new Product('002', 'A', "Orange", 'url(orange.jpg)', '25lei/kg');
+const avocado = new Product('003', "A", 'Avocado', 'url(avocado.img)', '75lei/kg');
+
+apple.log();
+orange.log();
+avocado.log()
